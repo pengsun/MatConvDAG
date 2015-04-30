508b6caa6bcdd90ddeab4702b8e4ac92367183e6
peng sun <pengsun000@gmail.com>
2015-04-30 16:53:18 -0400

    add core/tf_norm_ms.m

---
+++ core/tf_norm_ms.m
@@ -0,0 +1,29 @@
+classdef tf_norm_ms < tf_i
+  %TF_NORM_MS Data value normalization by taking out mean and dividing the
+  %standard deviation
+  %   Normalize to the range [-1, +1]
+  
+  properties
+    v_mean; % typically [H,W,C]. value mean for each pixel  
+    v_std;  % typically [H,W,C]. value std (standard deviation) for each pixel
+  end
+  
+  methods
+    function ob = tf_norm_ms()
+      ob.i = n_data();
+      ob.o = n_data();
+    end % tf_norm_ms
+    
+    function ob = fprop(ob)
+      ob.o.a = bsxfun(@minus,   ob.i.a, ob.v_mu);
+      ob.o.a = bsxfun(@rdivide, ob.i.a, ob.v_std);
+    end 
+    
+    function ob = bprop(ob)
+      ob.i.d = bsxfun(@rdivide, ob.o.d, ob.v_std);
+    end 
+    
+  end % methods
+  
+end % classdef tf_norm_ms
+
